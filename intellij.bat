777c21de48606a5a02de72bc2d5d97fb69fccec8
Henry <lettinghenry0@gmail.com>
2022-12-30 20:29:48 +0300

    Update intellij.bat

--- intellij.bat
+++ intellij.bat
@@ -1,5 +1,5 @@
 @REM Jetbrains Toolbox installation
-cd C:\Users\letti\AppData\Local\JetBrains\Toolbox\apps\IDEA-C\ch-0\223.7571.182\bin
+cd C:\Users\<username here>\AppData\Local\JetBrains\Toolbox\apps\IDEA-C\ch-0\223.7571.182\bin
 
 @REM for 64bit windows
 start idea64.exe
